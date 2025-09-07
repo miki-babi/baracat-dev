5ca81b8e6bde53f490b079709d65199b736d6fc0
miki-babi <mikiyasshiferaw@gmail.com>
2025-09-07 08:50:05 +0300

    clean-footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,32 +1,7 @@
 <footer class="bg-black text-white border-t border-gray-100">
   <div class="flex flex-col md:flex-row justify-between">
     <div>
-      <h4 class="font-serif text-2xl text-[#d4af37]">{{ config('app.name', 'Aurelia') }}</h4>
-    </div>
-    <div>
-      <ul>
-        <li>Home</li>
-        <li>Shop</li>
-        <li>Products</li>
-        <li>Contact</li>
-        <li>Blog</li>
-      </ul>
-    </div>
-    <div>
-      <ul>
-        <li>Privacy Policy</li>
-        <li>Terms of Use</li>
-        <li>Shipping</li>
-        <li>Return Policy</li>
-      </ul>
-    </div>
-    <div>
-      <ul>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Twitter</li>
-        <li>Youtube</li>
-      </ul>
+      <h4 class="font-serif text-2xl text-[#d4af37]">{{ config('app.name', 'Baracat') }}</h4>
     </div>
   </div>
 </footer>
